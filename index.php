2d01b2bc8b5c31fa65b85a62cf1be728eb74dddc
Camiloruiz1911 <juancamiloruizforonda216@gmail.com>
2025-07-29 21:02:24 -0500

    lo que hice fue una tabla donde puse un comando de editar

--- index.php
+++ index.php
@@ -1,52 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <title>Pagina CR7</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CDN -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <style>
-    /* CSS para centrar el formulario */
-    .centered-form {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh; /* Ocupa toda la altura de la ventana */
-    }
-
-    .form-container {
-      width: 100%;
-      max-width: 500px; /* Máximo ancho del formulario */
-      padding: 10px;
-      background-color: #f8f9fa; /* Color de fondo para el formulario */
-      border-radius: 8px;
-      box-shadow: 0 3px 7px rgba(0, 0, 0, 0.1);
-    }
-  </style>
-  
-</head>
-<body>
-
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background-color:#0000FF;">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">CR7 🏆</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="#carrusel">Galería</a></li>
-          <li class="nav-item"><a class="nav-link" href="#estadisticas">Estadísticas</a></li>
-          <li class="nav-item"><a class="nav-link" href="#video">Video</Video></a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
+<?php include_once "layout/layout.php" ?>
   <!-- Carrusel -->
   <div id="carrusel" class="carousel slide mt-3" data-bs-ride="carousel">
     <div class="carousel-inner">
@@ -192,7 +144,7 @@
 
    
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade col-12" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
@@ -200,6 +152,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+        
 
      
       <table class="table table-dark">
@@ -214,6 +167,7 @@
       <th scope="col">telefono</th>
       <th scope="col">editar</th>
       <th scope="col">eliminar</th>
+     
     </tr>
   </thead>
 
@@ -235,14 +189,16 @@ if ($conexion){
    
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
+      <th scope="row"><?php echo $i?></th>
+      <td><?php echo $fila ["Nombre"];?></td>
+      <td><?php echo $fila ["Apellido"];?></td>
+      <td><?php echo $fila ["Edad"];?></td>
+      <td><?php echo $fila ["Correo"];?></td>
+      <td><?php echo $fila ["Telefono"];?></td>
+      <td><a href="Update.php" type="button" class="btn btn-success">Editar</a><td>
+      <td><td>
+
+     
   
     </tr>
     
